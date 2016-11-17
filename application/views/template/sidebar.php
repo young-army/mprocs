365290749a8e7efebbab012541696f13c751c03c
yohputra <ianputra232@gmail.com>
2016-11-17 16:47:18 +0700

    (Putra) ubah side bar

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -7,7 +7,7 @@
               <img src="<?php echo base_url();?>assets/images/sinarmas.png" class="img-circle" alt="User Image" />
             </div>
             <div class="pull-left info">
-              <h4 style="font-family:Khand">PT Sinarmas Mining</h4>
+              <h4 style="font-family:Khand"><!-- PT Sinarmas Mining --></h4>
 
               <!--a href="#"><i class="fa fa-circle text-success"></i> Online</a-->
             </div>
@@ -28,21 +28,23 @@
             <li class="">
               <a href="<?php echo base_url();?>">
                 <i class="fa fa-dashboard"></i> <span>Dashboard</span> 
-              </a></li>
-			 <li class="">
+              </a>
+              </li>
+       <!-- <li class="">
               <a href="<?php echo base_url();?>book/meeting_room">
                 <i class="fa fa-building-o"></i> <span>Meeting Room</span> 
               </a></li>
-			  <li class="">
+        <li class="">
               <a href="<?php echo base_url();?>book/operational_car">
                 <i class="fa fa-truck"></i> <span>Operational Car</span> 
               </a></li>
-			   <li class="">
+         <li class="">
               <a href="<?php echo base_url();?>book/stationery">
                 <i class="fa fa-rocket"></i> <span>Stationery</span> 
-              </a></li>
-			 <?php if($this->session->userdata('sess_login')->level==1){?>
-			<li class="treeview">
+              </a>
+        </li> -->
+       <?php if($this->session->userdata('sess_login')->level==1){?>
+          <!--  <li class="treeview">
               <a href="#">
                 <i class="fa fa-check-square-o"></i>
                 <span>Reservation & Approval</span>
@@ -52,7 +54,7 @@
                 <li><a href="<?php echo base_url();?>appres/operational_car"><i class="fa fa-circle-o"></i> Operational Car</a></li>
                 <li><a href="<?php echo base_url();?>appres/stationery"><i class="fa fa-circle-o"></i> Stationery</a></li>
               </ul>
-            </li>
+            </li> -->
             <li class="treeview">
               <a href="#">
                 <i class="fa fa-folder"></i>
@@ -60,12 +62,16 @@
                 <i class="fa fa-angle-left pull-right"></i>
               </a>
               <ul class="treeview-menu">
-                <li><a href="<?php echo base_url();?>master_data/meeting_room"><i class="fa fa-circle-o"></i> Meeting Room</a></li>
+                <!-- <li><a href="<?php echo base_url();?>master_data/meeting_room"><i class="fa fa-circle-o"></i> Meeting Room</a></li>
                 <li><a href="<?php echo base_url();?>master_data/operational_car"><i class="fa fa-circle-o"></i> Operational Car</a></li>
-                <li><a href="<?php echo base_url();?>master_data/stationery"><i class="fa fa-circle-o"></i> Stationery</a></li>
+                <li><a href="<?php echo base_url();?>master_data/stationery"><i class="fa fa-circle-o"></i> Stationery</a></li> -->
+                <li><a href="<?php echo base_url();?>master_data/company"><i class="fa fa-circle-o"></i> Company</a></li>
+                <li><a href="<?php echo base_url();?>master_data/department"><i class="fa fa-circle-o"></i> Department</a></li>
+                <li><a href="<?php echo base_url();?>master_data/vendor"><i class="fa fa-circle-o"></i> Vendor</a></li>
+                <li><a href="<?php echo base_url();?>master_data/material"><i class="fa fa-circle-o"></i> Material</a></li>
               </ul>
             </li>
-			<li class="treeview">
+      <li class="treeview">
               <a href="#">
                 <i class="fa fa-gears"></i>
                 <span>Setting</span>
@@ -76,7 +82,7 @@
                 <li><a href="<?php echo base_url();?>acl/menu"><i class="fa fa-circle-o"></i> ACL Menu</a></li>
               </ul>
             </li>
-			 <?php } ?>
+       <?php } ?>
           </ul>
         </section>
         <!-- /.sidebar -->
